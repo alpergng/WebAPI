a50e27ed0b0a5e24b901f1d1475aecdfbfbfb9f4
alpergng <gungor.alper@yandex.com>
2019-06-10 22:43:47 +0300

    First real commit

---
+++ MemberWebAPI/Models/Member.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MemberWebAPI.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public String Fullname { get; set; }
+        public int Age { get; set; }
+
+        public Member(int id, string fullname, int age)
+        {
+            this.Id = id;
+            this.Fullname = fullname;
+            this.Age = age;
+        }
+
+    }
+}
---
+++ MemberWebAPI/Repositories/MemberRepository.cs
@@ -0,0 +1,64 @@
+﻿using MemberWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MemberWebAPI.Repositories
+{
+    public class MemberRepository
+    {
+        private static Lazy<List<Member>> container = new Lazy<List<Member>>(() => Initialize(), true);
+
+        private static List<Member> Initialize()
+        {
+            return new List<Member>
+            {
+                new Member(1,"Burak TUNGUT",22),
+                new Member(2,"Gökhan GÖKALP",28)
+            };
+            
+        }
+
+        public static Member Add(string fullname, int age)
+        {
+            int lastId = container.Value.Max(m => m.Id);
+            Member newMember = new Member(lastId + 1, fullname, age);
+            container.Value.Add(newMember);
+
+            return newMember;
+        }
+
+        public static List<Member> Get()
+        {
+            return container.Value;
+        }
+
+        public static Member Get(int id)
+        {
+            return container.Value.SingleOrDefault(m => m.Id == id);
+        }
+
+        public static void Remove(int id)
+        {
+            container.Value.Remove(Get(id));
+        }
+
+        public static void Update(Member member)
+        {
+            Remove(member.Id);
+
+            container.Value.Add(member);
+        }
+
+        public static bool IsExist(int id)
+        {
+            return container.Value.Any(m => m.Id == id);
+        }
+
+        public static bool IsExist(String fullname)
+        {
+            return container.Value.Any(m => m.Fullname == fullname);
+        }
+    }
+}
